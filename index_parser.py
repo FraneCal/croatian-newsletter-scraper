797be5c3770bf7d51904c584a632c558d2c763e4
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2025-09-16 19:16:55 +0200

    Update index_parser.py

--- index_parser.py
+++ index_parser.py
@@ -1,10 +1,37 @@
 import json
+from bs4 import BeautifulSoup
+import os
 
-FILENAME = "htmls/20250916_2709151_33.html"
+FOLDER = "htmls"
 
 def read_files():
-    with open(FILENAME, "r", encoding="utf-8") as f:
-        print(f.read())
+    for filename in os.listdir(FOLDER):
+        filepath = os.path.join(FOLDER, filename)
+        with open(filepath, "r", encoding="utf-8") as f:
+            response = f.read()
 
+            soup = BeautifulSoup(response, 'html.parser')
+
+            title = soup.find("h1", class_="vijesti-text-parsed title js-main-title").getText().strip()
+            link = soup.find("link", rel="canonical")["href"]
+
+            # load old JSON (if it exists), otherwise start fresh
+            try:
+                with open("results.json", "r", encoding="utf-8") as f:
+                    data = json.load(f)
+            except FileNotFoundError:
+                data = []
+
+            # create a new entry with id
+            new_entry = {
+                "id": len(data) + 1,
+                "link": link,
+                "title": title,
+            }
+            data.append(new_entry)
+
+            # save back to file
+            with open("results.json", "w", encoding="utf-8") as f:
+                json.dump(data, f, ensure_ascii=False, indent=2)
 
 read_files()
