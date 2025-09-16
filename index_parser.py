b328498bf7d12cc5b266d4153ab604d1f72abc77
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2025-09-16 21:07:41 +0200

    Update index_parser.py

--- index_parser.py
+++ index_parser.py
@@ -1,10 +1,15 @@
 import json
 from bs4 import BeautifulSoup
 import os
+import re
+import time
 
 FOLDER = "htmls"
 
 def read_files():
+    start = time.time()
+    counter = 0
+
     for filename in os.listdir(FOLDER):
         filepath = os.path.join(FOLDER, filename)
         with open(filepath, "r", encoding="utf-8") as f:
@@ -12,8 +17,11 @@ def read_files():
 
             soup = BeautifulSoup(response, 'html.parser')
 
-            title = soup.find("h1", class_="vijesti-text-parsed title js-main-title").getText().strip()
+            title = soup.find("h1", class_="vijesti-text-parsed title js-main-title").getText(strip=True)
+            safe_title = re.sub(r'[\\/*?:"<>|]', "_", title)
             link = soup.find("link", rel="canonical")["href"]
+            published_date = soup.find("div", class_="flex-1").find_all("div")[-1].get_text(strip=True).replace("Zadnja nadopuna:", "").strip()
+            description = soup.find("div", class_="text-holder").get_text(strip=True)
 
             # load old JSON (if it exists), otherwise start fresh
             try:
@@ -24,14 +32,25 @@ def read_files():
 
             # create a new entry with id
             new_entry = {
-                "id": len(data) + 1,
+                # "id": len(data) + 1,
                 "link": link,
-                "title": title,
+                "title": safe_title,
+                "published date": published_date,
+                "description": description,
             }
-            data.append(new_entry)
+
+            # don't add if link already exists
+            if not any(entry["link"] == link for entry in data):
+                data.append(new_entry)
+                counter += 1
 
             # save back to file
             with open("results.json", "w", encoding="utf-8") as f:
                 json.dump(data, f, ensure_ascii=False, indent=2)
+
+    print(f"Added {counter} new file(s) to results.json")
+    end = time.time()
+
+    print(f"{end-start:.2f} seconds")
 
 read_files()
